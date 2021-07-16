6adcf3b3575031dc474f35b35eb396019a8e3690
marijntepas <marijntepas@gmail.com>
2021-07-16 14:17:06 +0200

    Registered packethandler

--- src/main/java/com/momo/mediaoverlay/client/ClientProxy.java
+++ src/main/java/com/momo/mediaoverlay/client/ClientProxy.java
@@ -1,6 +1,8 @@
 package com.momo.mediaoverlay.client;
 
 import com.momo.mediaoverlay.CommonProxy;
+import com.momo.mediaoverlay.MediaOverlay;
+import com.momo.mediaoverlay.PacketHandlerClient;
 import com.momo.mediaoverlay.client.gui.GuiMedia;
 
 import net.minecraft.client.Minecraft;
@@ -10,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ClientProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent event) {
-
+    	MediaOverlay.Channel.register(new PacketHandlerClient());
     }
 
     @Override
